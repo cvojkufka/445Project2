eaeac01256097218a959e76741e0fec77b1b3933
Christopher Vojkufka <christopher.vojkufka@bsu.edu>
2026-04-08 22:36:12 -0400

    learn page format

--- src/components/pages/Learn.vue
+++ src/components/pages/Learn.vue
@@ -1,13 +1,73 @@
 <template>
-    <div>
-    <h1>Learn Page</h1>
-    <p>This is where learn content will go.</p>
-    <button @click="goHome">Back to Home</button>
+    <div class="header">
+    <img :src="banner" alt="bannerLogo" />
+    <h2 class="overlay-text">Learn!</h2>
+  </div>
+
+  <div class="home">
+    <button @click="goHome">
+      <img :src="home" :alt="home">
+    </button>
   </div>
 </template>
 
 <script setup>
+import banner from '../../assets/Banner.png'
+import home from '../../assets/Home.png'
+
     defineProps ({
         goHome: Function
     })
 </script>
+
+<style>
+/*Banner*/
+.header { 
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: fit-content;
+}
+
+.header img {
+  width: 100%;
+}
+
+.overlay-text {
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: rgb(255, 255, 255);
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+/*Home Button*/
+.home {
+  display: flex;
+  align-items: center;
+
+  height: 100px;
+  width: 100px;
+
+  border-radius: 100px;
+  border: 2px solid black;
+  background: white;
+
+  overflow: hidden;
+  cursor: pointer;
+  justify-content: center;
+  top: 50px;
+  left: 150px;
+  position: absolute;
+}
+
+.home img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  transform: scale(1.7);
+}
+</style>
